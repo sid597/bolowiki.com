5781de32c80f47a742bf92affda708dcdf12f0ca
sid597 <siddharthdv77@gmail.com>
2020-06-10 13:24:32 +0530

    db queries checking

--- querydb.py
+++ querydb.py
@@ -12,32 +12,32 @@ class queryDb():
         self.c = c
 
     def getUserData(self):
-        c.execute("SELECT * FROM users WHERE username = (%s)",
+        self.c.execute("SELECT * FROM users WHERE username = (%s)",
                   (thwart(self.username),))
 
     def getUserId(self):
-        c.execute("SELECT id  FROM users WHERE username = (%s)",
+        self.c.execute("SELECT id  FROM users WHERE username = (%s)",
                   (thwart(self.username),))
 
     def createNewUser(self,username, password, email,):
-        c.execute("INSERT INTO users (username, password, email, tracking) VALUES (%s, %s, %s, %s)",
+        self.c.execute("INSERT INTO users (username, password, email, tracking) VALUES (%s, %s, %s, %s)",
                   (thwart(self.username), thwart(password), thwart(email), thwart("/introduction-to-python-programming/")))
 
     def getUserWikilinks(self):
         userId = self.getUserId()
-        userLinks = c.execute("SELECT wikiLinks FROM userWikiLinks WHERE userId=(%s)",
+        userLinks = self.c.execute("SELECT wikiLinks FROM userWikiLinks WHERE userId=(%s)",
                               (thwart(userId),))
         return userLinks
 
     def checkLinkInGlobalWikiList(self):
-        isItThere = c.execute("SELECT id FROM wikiList WHERE article=(%s) ",
+        isItThere = self.c.execute("SELECT id FROM wikiList WHERE article=(%s) ",
                               (thwart(self.wikiArticleFullLink),))
         if isItThere is None:
             return False
         return isItThere
 
     def getWikiLocation(self):
-        location = c.execute("SELECT location FROM wikiList WHERE article=(%s)",
+        location = self.c.execute("SELECT location FROM wikiList WHERE article=(%s)",
                              (thwart(self.wikiArticleFullLink),))
         if location is None:
             return False
@@ -48,7 +48,7 @@ class queryDb():
         locationToSaveInto = parsedUrl.path
         wikipediaPageLink = parsedUrl.scheme+'://'+parsedUrl.netloc + parsedUrl.path
         try:
-            c.execute("INSERT INTO wikiList (location, article) VALUES (%s, %s) ",
+            self.c.execute("INSERT INTO wikiList (location, article) VALUES (%s, %s) ",
                       (thwart(locationToSaveInto), thwart(wikipediaPageLink),))
         except Exception as e:
             return str(e)
