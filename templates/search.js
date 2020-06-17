b6ea0bffaad3387229c3d6a685fc3745bb01bfa2
sid597 <siddharthdv77@gmail.com>
2020-06-17 19:15:28 +0530

    Added search for wikipedia articles. Current behaviour : 1. Searches wikipedia for articles in search query 2. Shows the suggestions for them
    TODO :
    1. Implement speech api to convert speech to text for search
    2. when clicked on a suggestion Link what happens, when pressed entered
       what happens ?
    3. Testing and corner cases

---
+++ templates/search.js
@@ -0,0 +1,130 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    searchMainDiv = document.querySelector('#searchMainDiv')
+    searchBox = document.querySelector('#searchInputBox')
+    searchSuggestion = document.querySelector('#searchSuggestion')
+    searchSuggestionList =document.querySelector('#searchSuggestionList')
+    mainDiv = document.querySelector('#mainDiv')
+
+
+    function defaultStyle() {
+        console.log(searchBox.value)
+        if (!searchBox.value) {
+            searchMainDiv.style.borderRadius = "24px"
+            searchMainDiv.style.boxShadow = 'none'
+            searchMainDiv.style.borderColor = "rgb(223, 225, 229)"
+            searchSuggestion.style.display = 'none'
+        }
+        else {
+            searchMainDiv.style.borderRadius = "24px"
+            searchMainDiv.style.boxShadow = "0 1px 6px 0 rgba(32,33,36,0.28)";
+            searchMainDiv.style.borderColor = "rgba(223,225,229,0)";
+            searchSuggestion.style.display = 'none'
+        }
+    }
+
+    function focusedStyle() {
+        if (searchBox.value) {
+            searchMainDiv.style.borderBottomRightRadius = 0
+            searchMainDiv.style.borderBottomLeftRadius = 0
+            searchMainDiv.style.boxShadow = "0 1px 6px 0 rgba(32,33,36,0.28)"
+            searchMainDiv.style.borderColor = "rgba(223,225,229,0)"
+            searchSuggestion.style.display = 'flex'
+        }
+        else {
+            searchMainDiv.style.borderRadius = "24px"
+            searchMainDiv.style.boxShadow = "0 1px 6px 0 rgba(32,33,36,0.28)";
+            searchMainDiv.style.borderColor = "rgba(223,225,229,0)";
+            searchSuggestion.style.display = 'none'
+        }
+    }
+
+    function getMatchString(s1, s2) {
+        l1 = s1.toLowerCase()
+        l2 = s2.toLowerCase()
+        // console.log(s1,'---', s2)
+        if (l1 === l2) { return s2; }
+
+        res = ""
+        for (x = 0; x <= s1.length; x++) {
+            if (l1[x] == l2[x]) {
+                res += s2[x];
+            }
+            else {
+                break;
+            }
+        }
+        return res;
+
+    }
+
+    function getWikipediaresponse(searchText) {
+
+        const wikiLink = `https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&formatversion=2&search=${searchText}&namespace=0&limit=10`
+
+        const request = new XMLHttpRequest()
+        request.open('GET', wikiLink);
+
+        request.onload = () => {
+            const data = JSON.parse(request.responseText);
+            // console.log(data)
+            var l = ['<ul class="list-group list-group-flush">'];
+            // console.log(l)
+
+            for (var i = 0; i < 10; i++) {
+
+                textLink = data[3][i]
+                text = data[1][i]
+                if (text) {
+                    matchText = getMatchString(searchText, text)
+                    unmatchedText = text.slice(matchText.length, text.length)
+                    // console.log(`${searchText}-->`, matchText, '||', unmatchedText)
+                    if (matchText) {
+                        if (unmatchedText) {
+                            l.push(`<a class="resultLink" href=${textLink}> <li class="listitem"><b>${matchText}</b>${unmatchedText}</li></a>`)
+                        }
+                        else {
+                            l.push(`<a class="resultLink" href=${textLink}> <li class="listitem"><b>${matchText}</b></li></a>`)
+                        }
+                    }
+                    // document.querySelector('.suggestions').append(ii)
+                }
+            };
+            l.push('</ul>')
+            // console.log(l.join(''))
+            searchSuggestionList.innerHTML = l.join('')
+            // document.querySelector('.suggestions').innerHTML = l.join('')
+            return l.join('');
+
+
+        };
+        request.send(null)
+        // console.log(request.responseText)
+    }
+
+    function mouseoverMainDiv() {
+        searchMainDiv.style.boxShadow = "0 1px 6px 0 rgba(32,33,36,0.28)";
+        searchMainDiv.style.borderColor = "rgba(223,225,229,0)";
+    }
+
+    mainDiv.addEventListener('mouseover', e => {
+        mouseoverMainDiv()
+    });
+
+    searchBox.addEventListener('keyup', e => {
+        focusedStyle()
+        if (e.target.value) { 
+            getWikipediaresponse(e.target.value) 
+        }
+    });
+
+    document.addEventListener('click', e => {
+        clickVal = mainDiv.contains(e.target)
+        if (clickVal) {
+            focusedStyle()
+        }
+        else {
+            defaultStyle()
+        }
+    });
+});
