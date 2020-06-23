517d63e60b156e0aa246aa141c0e2d336755529c
sid597 <siddharthdv77@gmail.com>
2020-06-24 02:25:33 +0530

    Changes in JS : 1. Added voice search feature 2. Show voice search Icon only in chrome 3. On mouse move out of search box return to normal view 4. Can search using search Icon 5. Made code somewhat more modular 6. Formated code to keep functions with relation in a group
    Not sure why I am not reformatiing code in their individual files Is it
    because I like it this way or is it because of more work and time.
    
    Changes in CSS:
    1. Changed colors for the search button and its height
    
    Changes in HTML:
    1. Added modal for voice Search
    2. Moved search icon next to voice icon
    3. Added a bar for asthetics
    
    Also added a test search html file for testing it in isolation

--- static/js/search.js
+++ static/js/search.js
@@ -1,20 +1,130 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const queryTopResultLink = null;
-  const queryTopResultText = null;
+/* eslint-disable no-use-before-define */
+document.addEventListener('DOMContentLoaded', async () => {
+  const isChrome = !!window.chrome;
+  let queryTopResultLink = null;
+  let queryTopResultText = null;
+  let voiceSearchQuery = null;
+  const searchIcon = document.querySelector('#searchSVG');
   const searchMainDiv = document.querySelector('#searchMainDiv');
   const searchBox = document.querySelector('#searchInputBox');
+  const searchLanguage = document.querySelector('#searchLanguage');
   const searchSuggestion = document.querySelector('#searchSuggestion');
   const searchSuggestionList = document.querySelector('#searchSuggestionList');
   const mainDiv = document.querySelector('#mainDiv');
   const audioModalContentChildren = document.querySelector('#audioModalContent').children;
   const audioModalHeader = document.querySelector('#audioModalHeader');
   const wikipediaAccordian = document.querySelector('#wikipediaAccordian');
+  const voiceSearchModal = new bootstrap.Modal(document.querySelector('#voiceSearchModal'));
+  const voiceSearchIcon = document.querySelector('#voiceSearch');
+  // const voiceSearchError = document.querySelector('#voiceSearchError');
+  const voiceSearchCurrentStatus = document.querySelector('#voiceSearchCurrentStatus');
+  const voiceSearchResult = document.querySelector('#voiceSearchCurrentStatus');
+
+  if (!isChrome) {
+    voiceSearchIcon.style.display = 'none';
+  }
+  // #############################################################################
+  // ## Voice search related Functions                                          ##
+  // #############################################################################
+
+  function showVoiceSearchError(msg) {
+    voiceSearchCurrentStatus.className = 'alert alert-danger';
+    voiceSearchCurrentStatus.innerHTML = msg;
+  }
+
+  function showVoiceSearchNormalMsg(msg) {
+    voiceSearchCurrentStatus.className = 'alert alert-light';
+    voiceSearchCurrentStatus.innerHTML = msg;
+  }
+
+  function addVoiceQuery() {
+    searchBox.value = voiceSearchQuery;
+    showSuggestionsForQueryInInputBox(voiceSearchQuery);
+  }
+
+  voiceSearchIcon.addEventListener('click', () => {
+    console.debug('Hello');
+
+    listenToVoiceQuery();
+  });
+
+  async function listenToVoiceQuery() {
+    const listening = false;
+    console.debug('inside listen to voice query');
+    if (!('webkitSpeechRecognition' in window)) {
+      // TODO : Show a update to chrome message or completly hide this svg
+    } else {
+      const recognition = await new webkitSpeechRecognition();
+      // recognition.continuous = true;
+      recognition.interimResults = true;
+      recognition.lang = searchLanguage;
+      console.debug(recognition);
+
+      recognition.onstart = (e) => {
+        showVoiceSearchNormalMsg('Listening ...');
+        console.debug(`recognition started ${e}`);
+      };
+
+      recognition.onresult = (e) => {
+        console.debug(`recognition complete here is your result ${e.results[0][0].transcript}`);
+        console.debug(`here is your event ${e}`);
+        const trans = e.results[0][0].transcript;
+
+        console.debug(`recognition complete here is your result ${trans}`);
+        voiceSearchResult.innerHTML = trans;
+        voiceSearchQuery = trans;
+        return trans;
+      };
+
+      recognition.onerror = (e) => {
+        if (e.error === 'no-speech') {
+          showVoiceSearchError('No speech was detected. You may need to adjust your  microphone');
+        }
+        if (e.error === 'audio-capture') {
+          showVoiceSearchError('No microphone was found. Ensure that a microphone is installed and that microphone settings</a> are configured correctly.');
+        }
+        if (e.error === 'not-allowed') {
+          showVoiceSearchError('Permission to use microphone was denied/blocked. To change,go to chrome://settings/contentExceptions#media-stream');
+        }
+
+        console.debug(` error occured : ${e.error}`);
+      };
+      recognition.onend = async () => {
+        console.debug('ended');
+        setTimeout(() => {
+          showVoiceSearchNormalMsg('Closing voice search');
+          voiceSearchModal.hide();
+          addVoiceQuery();
+        }, 3000);
+      };
+
+      recognition.start();
+    }
+  }
+
+  // #############################################################################
+  // ## Audio Modal for search query result
+  // #############################################################################
+  function showAudioModal(mediaLocation, articleText, articleWikiLink) {
+    // console.debug(`media location is : ${mediaLocation} and articleText is ${articleText}`)
+    const audioModalBody = audioModalContentChildren[1];
+    const audioModalFooter = audioModalContentChildren[2];
+    // console.debug(audioModalHeader, audioModalBody, audioModalFooter)
+    audioModalHeader.innerHTML = `<audio controls style="width: 100%;"><source src="${mediaLocation}" type="audio/mpeg" />Your browser does not support the audio element.</audio>`;
+    audioModalBody.innerHTML = articleText;
+    audioModalFooter.innerHTML = `<a href="${articleWikiLink}">${articleWikiLink}</a>`;
+    const myModal = new bootstrap.Modal(document.querySelector('#audioModal'));
+    myModal.toggle();
+  }
+
+  // #############################################################################
+  // ## Show wikipedia article accordian
+  // #############################################################################
 
   function createWikipediaContentLinks(wikiContentText) {
     const wikiContentTextJoined = wikiContentText.split(' ').join('_');
     const fullWikiLink = `${queryTopResultLink}#${wikiContentTextJoined}`;
     // console.debug(fullWikiLink)
-
     return fullWikiLink;
   }
 
@@ -32,18 +142,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     l.push('</ul>');
     return l.join('');
-  }
-
-  function showAudioModal(mediaLocation, articleText, articleWikiLink) {
-    // console.debug(`media location is : ${mediaLocation} and articleText is ${articleText}`)
-    const audioModalBody = audioModalContentChildren[1];
-    const audioModalFooter = audioModalContentChildren[2];
-    // console.debug(audioModalHeader, audioModalBody, audioModalFooter)
-    audioModalHeader.innerHTML = `<audio controls style="width: 100%;"><source src="${mediaLocation}" type="audio/mpeg" />Your browser does not support the audio element.</audio>`;
-    audioModalBody.innerHTML = articleText;
-    audioModalFooter.innerHTML = `<a href="${articleWikiLink}">${articleWikiLink}</a>`;
-    const myModal = new bootstrap.Modal(document.getElementById('audioModal'));
-    myModal.toggle();
   }
 
   function createNewAccordianCard(articleTitle, articleContents) {
@@ -68,6 +166,9 @@ document.addEventListener('DOMContentLoaded', () => {
     //   console.debug(newCard)
   }
 
+  // #############################################################################
+  // ## Make requests
+  // #############################################################################
   function getAudioFileData(functionToHandleTheResponse, articleWikiLink) {
     console.debug('inside getAudioFileData');
     console.debug(`function to handle response is ${functionToHandleTheResponse}`);
@@ -93,6 +194,67 @@ document.addEventListener('DOMContentLoaded', () => {
     request.send(data);
   }
 
+  function matchString(s1, s2) {
+    const l1 = s1.toLowerCase();
+    const l2 = s2.toLowerCase();
+    // console.debug(s1,'---', s2)
+    if (l1 === l2) { return s2; }
+
+    let res = '';
+    for (let x = 0; x <= s1.length; x += 1) {
+      if (l1[x] === l2[x]) {
+        res += s2[x];
+      } else {
+        break;
+      }
+    }
+    return res;
+  }
+  function getWikipediaresponse(searchText) {
+    const wikiLink = `https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&formatversion=2&search=${searchText}&namespace=0&limit=10`;
+
+    const request = new XMLHttpRequest();
+    request.open('GET', wikiLink);
+
+    request.onload = () => {
+      const data = JSON.parse(request.responseText);
+      // console.debug(` data from wikipedia response is ${data}`);
+      const l = ['<ul class="list-group list-group-flush">'];
+      // console.debug(l)
+
+      for (let i = 0; i < 10; i += 1) {
+        const textLink = data[3][i];
+        const text = data[1][i];
+        if (text) {
+          const matchText = matchString(searchText, text);
+          const unmatchedText = text.slice(matchText.length, text.length);
+          // console.debug(`${searchText}-->`, matchText, '||', unmatchedText)
+          if (matchText) {
+            if (unmatchedText) {
+              l.push(`<a class="resultLink" href=${textLink}> <li class="listitem"><span style="font-weight:600">${matchText}</span><span>${unmatchedText}</span></li></a>`);
+            } else {
+              l.push(`<a class="resultLink" href=${textLink}> <li class="listitem"><b>${matchText}</b></li></a>`);
+            }
+          }
+          // document.querySelector('.suggestions').append(ii)
+        }
+      }
+      l.push('</ul>');
+      // console.debug(l.join(''))
+      createSuggestionList(l);
+      // document.querySelector('.suggestions').innerHTML = l.join('')
+      queryTopResultLink = data[3][0];
+      // console.debug(queryTopResultLink)
+      queryTopResultText = data[1][0];
+      return l.join('');
+    };
+    request.send(null);
+    // console.debug(request.responseText)
+  }
+
+  // #############################################################################
+  // ## Search query and suggestion List
+  // #############################################################################
   function defaultStyle() {
     // console.debug(`searchBox.value is ${searchBox.value}`)
     if (!searchBox.value) {
@@ -123,63 +285,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  function getMatchString(s1, s2) {
-    const l1 = s1.toLowerCase();
-    const l2 = s2.toLowerCase();
-    // console.debug(s1,'---', s2)
-    if (l1 === l2) { return s2; }
-
-    let res = '';
-    for (let x = 0; x <= s1.length; x += 1) {
-      if (l1[x] === l2[x]) {
-        res += s2[x];
-      } else {
-        break;
-      }
-    }
-    return res;
-  }
-
-  function getWikipediaresponse(searchText) {
-    const wikiLink = `https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&formatversion=2&search=${searchText}&namespace=0&limit=10`;
-
-    const request = new XMLHttpRequest();
-    request.open('GET', wikiLink);
-
-    request.onload = () => {
-      const data = JSON.parse(request.responseText);
-      // console.debug(data)
-      const l = ['<ul class="list-group list-group-flush">'];
-      // console.debug(l)
-
-      for (let i = 0; i < 10; i += 1) {
-        const textLink = data[3][i];
-        const text = data[1][i];
-        if (text) {
-          const matchText = getMatchString(searchText, text);
-          const unmatchedText = text.slice(matchText.length, text.length);
-          // console.debug(`${searchText}-->`, matchText, '||', unmatchedText)
-          if (matchText) {
-            if (unmatchedText) {
-              l.push(`<a class="resultLink" href=${textLink}> <li class="listitem"><span style="font-weight:600">${matchText}</span><span>${unmatchedText}</span></li></a>`);
-            } else {
-              l.push(`<a class="resultLink" href=${textLink}> <li class="listitem"><b>${matchText}</b></li></a>`);
-            }
-          }
-          // document.querySelector('.suggestions').append(ii)
-        }
-      }
-      l.push('</ul>');
-      // console.debug(l.join(''))
-      searchSuggestionList.innerHTML = l.join('');
-      // document.querySelector('.suggestions').innerHTML = l.join('')
-      queryTopResultLink = data[3][0];
-      // console.debug(queryTopResultLink)
-      queryTopResultText = data[1][0];
-      return l.join('');
-    };
-    request.send(null);
-    // console.debug(request.responseText)
+  function createSuggestionList(listToShow) {
+    searchSuggestionList.innerHTML = listToShow.join('');
   }
 
   function mouseoverMainDiv() {
@@ -187,21 +294,63 @@ document.addEventListener('DOMContentLoaded', () => {
     searchMainDiv.style.borderColor = 'rgba(223,225,229,0)';
   }
 
+  function mouseoutMainDiv() {
+    searchMainDiv.style.boxShadow = 'none';
+    searchMainDiv.style.borderColor = 'rgb(223, 225, 229)';
+  }
+
+  function searchQueryInsideInputBox(actionType, query) {
+    console.debug(`query for search is ${query}`);
+    defaultStyle();
+    const searchQuery = query.trim();
+    if (searchQuery) {
+      getAudioFileData(actionType, query);
+    }
+    // else {
+
+    // }
+  }
+
+  function showSuggestionsForQueryInInputBox(query) {
+    console.debug(`query for suggestion is ${query}`);
+    focusedStyle();
+    if (query) {
+      const searchQuery = query.trim();
+      if (searchQuery) {
+        getWikipediaresponse(searchQuery);
+      }
+    }
+  }
+
+  searchIcon.addEventListener('click', () => {
+    searchQueryInsideInputBox('both', searchBox.value);
+  });
+
   mainDiv.addEventListener('mouseover', () => {
     mouseoverMainDiv();
+  });
+  mainDiv.addEventListener('mouseout', () => {
+    console.debug(searchBox.value);
+    if (searchBox.value === '') {
+      mouseoutMainDiv();
+    } else {
+      mouseoverMainDiv();
+    }
   });
 
   searchBox.addEventListener('keyup', (e) => {
     if (e.key === 'Enter') {
       console.debug('keyup event with enter key');
-      defaultStyle();
-      getAudioFileData('both', queryTopResultLink);
+      searchQueryInsideInputBox('both', queryTopResultLink);
     } else {
-      focusedStyle();
-      const searchQuery = (e.target.value).trim();
-      if (searchQuery) {
-        getWikipediaresponse(searchQuery);
-      }
+      const query = e.target.value;
+      showSuggestionsForQueryInInputBox(query);
+    }
+  });
+
+  searchBox.addEventListener('input', (e) => {
+    if (typeof e.data === 'undefined') {
+      showSuggestionsForQueryInInputBox(e.data);
     }
   });
 
