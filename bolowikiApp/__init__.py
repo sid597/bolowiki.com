319b60cc984c90d4257a7f3963a3f80ef19defa8
sid597 <siddharthdv77@gmail.com>
2020-08-08 00:51:19 +0530

    added logging in gunicorn

--- bolowikiApp/__init__.py
+++ bolowikiApp/__init__.py
@@ -11,9 +11,12 @@ from bolowikiApp.models import db
 from bolowikiApp.config import Config
 
 
-logging.basicConfig(filename='error.log', level=logging.DEBUG)
 migrate = Migrate()
 
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
 
 def create_app(config_class=Config):
     app = Flask(__name__, template_folder='templates')
