26e1e2963af0f3f546b9e622a1d8b91aa7efd1e1
sid597 <siddharthdv77@gmail.com>
2021-01-07 16:02:39 +0000

    added dotenv support for environment varialbes and secrets, removed text size because postgres dies not support that

--- bolowikiApp/config.py
+++ bolowikiApp/config.py
@@ -4,6 +4,6 @@ import os
 
 class Config:
     EXPLAIN_TEMPLATE_LOADING = True
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
+    SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')
